2eed1daefaa7ba1ac2ec049df7bfff594a3fb8e5
ak47ajhi <116179889+ak47ajhi@users.noreply.github.com>
2022-10-24 22:25:17 +0500

    555视频去广告

---
+++ 555.js
@@ -0,0 +1,16 @@
+/******************************
+
+555视频
+
+*******************************
+
+[rewrite_local]
+
+^https:\/\/vip7\.fzwdyy\.cn:8083\/api\/getAdvertInfo url reject
+
+
+[mitm] 
+
+hostname = vip7.fzwdyy.cn
+
+*******************************/
